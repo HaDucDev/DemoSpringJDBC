2e420fa79ed94c2ee9ffa8580e2cb9ecf6610890
HaDucDev <haductrung392000@gmail.com>
2022-11-27 23:01:28 +0700

    JdbcTemplate-getList-RowMapper

--- DemoSpringJDBC/src/main/java/Main.java
+++ DemoSpringJDBC/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +12,12 @@ public class Main {
         ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
         DataSource dataSource= (DataSource) context.getBean("dataSource");
         JdbcTemplate template= new JdbcTemplate(dataSource);
-        template.update("UPDATE product SET description=? WHERE id=?","quá chất nhé",12);// chu thuong nah. truy van truc tiep vs csdl
+        //template.update("UPDATE product SET description=? WHERE id=?","quá chất nhé",12);// chu thuong nah. truy van truc tiep vs csdl
 
+        List<String> a = template.query("SELECT * FROM category", (rs, i) -> {
+            return rs.getString("name");
+        });
+
+        a.forEach( v -> System.out.println(v));
     }
 }
