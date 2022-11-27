9d3214ad2d263cbf07435de86160bfc9fa36db81
HaDucDev <haductrung392000@gmail.com>
2022-11-28 01:35:15 +0700

    jdbcTemplate

--- DemoSpringJDBC/src/main/java/Main.java
+++ DemoSpringJDBC/src/main/java/Main.java
@@ -9,22 +9,35 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
-        DataSource dataSource= (DataSource) context.getBean("dataSource");
-        JdbcTemplate template= new JdbcTemplate(dataSource);
-        //template.update("UPDATE product SET description=? WHERE id=?","quá chất nhé",12);// chu thuong nah. truy van truc tiep vs csdl
-
-//        List<String> a = template.query("SELECT * FROM category", (rs, i) -> {
-//            return rs.getString("name");
-//        });
+//        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+//        DataSource dataSource= (DataSource) context.getBean("dataSource");
+//        JdbcTemplate template= new JdbcTemplate(dataSource);
+//        //template.update("UPDATE product SET description=? WHERE id=?","quá chất nhé",12);// chu thuong nah. truy van truc tiep vs csdl
 //
-//        a.forEach( v -> System.out.println(v));
+////        List<String> a = template.query("SELECT * FROM category", (rs, i) -> {
+////            return rs.getString("name");
+////        });
+////
+////        a.forEach( v -> System.out.println(v));
+//
+//        List<String> b = template.query("SELECT * FROM category where name LIKE CONCAT('%',?,'%')",
+//                                                (rs, i) -> {return rs.getString("name");},
+//                                                "máy"
+//                                                );
+//
+//        b.forEach( v -> System.out.println(v));
+
+
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+        JdbcTemplate template= (JdbcTemplate) context.getBean("jdbcTemplate");
 
         List<String> b = template.query("SELECT * FROM category where name LIKE CONCAT('%',?,'%')",
-                                                (rs, i) -> {return rs.getString("name");},
-                                                "máy"
-                                                );
+                (rs, i) -> {return rs.getString("name");},
+                "máy"
+        );
 
         b.forEach( v -> System.out.println(v));
+
     }
 }
