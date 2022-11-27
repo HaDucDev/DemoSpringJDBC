93209a231246d3d23c99354ee618516aecd5e164
HaDucDev <haductrung392000@gmail.com>
2022-11-27 22:21:25 +0700

    update-JDBCTemPlate

--- DemoSpringJDBC/src/main/java/Main.java
+++ DemoSpringJDBC/src/main/java/Main.java
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
-        DataSource dataSource= (DataSource) context.getBean("dataSouruce");
+        DataSource dataSource= (DataSource) context.getBean("dataSource");
+        JdbcTemplate template= new JdbcTemplate(dataSource);
+        template.update("UPDATE product SET description=? WHERE id=?","quá chất nhé",12);// chu thuong nah. truy van truc tiep vs csdl
 
     }
 }
